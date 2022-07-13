9566ac11637db02354d6f60db89afe55aea10729
lewah <ingridlewah@gmail.com>
2022-07-13 17:53:01 +0300

    0x08. Python - More Classes and Objects

---
+++ 1.py
@@ -0,0 +1,8 @@
+def uniq(list):
+    """ Returns unique values of a list """
+    u_list = []
+    for item in list:
+        if item not in u_list:
+            u_list.append(item)
+    return u_list
+print(uniq.u_list)
